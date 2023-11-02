2f125a743a7eb7e621a6a8b94ae0f449054507ca
joonsuk12 <1004joonsuk@naver.com>
2023-11-02 20:55:09 +0900

    feature: 피드백 반영

--- booklog/src/main/java/com/booklog/member/controller/MemberController.java
+++ booklog/src/main/java/com/booklog/member/controller/MemberController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member2")
+@RequestMapping("/v2/member")
 // TODO Authentication 처리 필요
 public class MemberController {
 
