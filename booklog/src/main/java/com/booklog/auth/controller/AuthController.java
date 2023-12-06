8ce19154ad767ad78e26553cee9de0e9700b166a
joonsuk12 <1004joonsuk@naver.com>
2023-12-06 16:29:44 +0900

    feature: Post로 변경해 code 보호

--- booklog/src/main/java/com/booklog/auth/controller/AuthController.java
+++ booklog/src/main/java/com/booklog/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.booklog.auth.controller;
 
+import com.booklog.auth.oauth.common.OAuthReqDto;
 import com.booklog.auth.oauth.google.GoogleService;
 import com.booklog.auth.oauth.google.GoogleTokenDto;
 import com.booklog.auth.oauth.google.GoogleUserDto;
@@ -8,15 +9,13 @@ import com.booklog.auth.oauth.kakao.KakaoTokenDto;
 import com.booklog.auth.oauth.kakao.KakaoUserDto;
 import com.booklog.member.dto.MemberLoginDto;
 import com.booklog.member.entity.Member;
-import java.net.URI;
-import java.net.URISyntaxException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -29,49 +28,19 @@ public class AuthController {
     private final KakaoService kakaoService;
     private final GoogleService googleService;
 
-    @GetMapping("/kakaoLogin")
-    public ResponseEntity<MemberLoginDto> kakaoLogin(@RequestParam("code") String code)
-        throws URISyntaxException {
-        try {
-            KakaoTokenDto kakaoTokenDto = kakaoService.getKakaoAccessToken(code);
-            KakaoUserDto kakaoUserDto = kakaoService.getKakaoUser(kakaoTokenDto.getAccessToken());
-            Member loginMember = kakaoService.loginKakao(kakaoUserDto);
-            URI redirectUri = new URI("http://localhost:3000");
-
-            // ResponseEntity를 사용하여 리다이렉트를 반환
-            return ResponseEntity.status(HttpStatus.FOUND)
-                .location(redirectUri)
-                .body(kakaoService.getMemberLoginDto(loginMember));
-        } catch (Exception e) {
-            URI errorRedirectUri = new URI("http://localhost:3000");
-
-            // ResponseEntity를 사용하여 에러 상태와 리다이렉트를 반환
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .location(errorRedirectUri)
-                .build();
-        }
+    @PostMapping("/kakaoLogin")
+    public ResponseEntity<MemberLoginDto> kakaoLogin(@RequestBody OAuthReqDto oAuthReqDto) {
+        KakaoTokenDto kakaoTokenDto = kakaoService.getKakaoAccessToken(oAuthReqDto.getCode());
+        KakaoUserDto kakaoUserDto = kakaoService.getKakaoUser(kakaoTokenDto.getAccessToken());
+        Member loginMember = kakaoService.loginKakao(kakaoUserDto);
+        return new ResponseEntity<>(kakaoService.getMemberLoginDto(loginMember), HttpStatus.OK);
     }
 
-    @GetMapping("/googleLogin")
-    public ResponseEntity<MemberLoginDto> googleLogin(@RequestParam("code") String code)
-        throws URISyntaxException {
-        try{
-            GoogleTokenDto googleTokenDto = googleService.getGoogleAccessToken(code);
-            GoogleUserDto googleUserDto = googleService.getGoogleUser(googleTokenDto.getAccessToken());
-            Member loginMember = googleService.loginGoogle(googleUserDto);
-            URI redirectUri = new URI("http://localhost:3000");
-
-            // ResponseEntity를 사용하여 리다이렉트를 반환
-            return ResponseEntity.status(HttpStatus.FOUND)
-                .location(redirectUri)
-                .body(kakaoService.getMemberLoginDto(loginMember));
-        } catch (Exception e) {
-            URI errorRedirectUri = new URI("http://localhost:3000");
-
-            // ResponseEntity를 사용하여 에러 상태와 리다이렉트를 반환
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .location(errorRedirectUri)
-                .build();
-        }
+    @PostMapping("/googleLogin")
+    public ResponseEntity<MemberLoginDto> googleLogin(@RequestBody OAuthReqDto oAuthReqDto) {
+        GoogleTokenDto googleTokenDto = googleService.getGoogleAccessToken(oAuthReqDto.getCode());
+        GoogleUserDto googleUserDto = googleService.getGoogleUser(googleTokenDto.getAccessToken());
+        Member loginMember = googleService.loginGoogle(googleUserDto);
+        return new ResponseEntity<>(kakaoService.getMemberLoginDto(loginMember), HttpStatus.OK);
     }
 }
