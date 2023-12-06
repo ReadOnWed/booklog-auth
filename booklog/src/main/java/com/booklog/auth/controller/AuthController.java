9c40d6dd6a0b853e53cab7440ab9591f17c52c28
joonsuk12 <1004joonsuk@naver.com>
2023-12-06 13:15:09 +0900

    feature: 토큰 검증 로직 삭제, 메인 페이지로 리다이렉트하고 useEffect로 처리하도록 코드 수정

--- booklog/src/main/java/com/booklog/auth/controller/AuthController.java
+++ booklog/src/main/java/com/booklog/auth/controller/AuthController.java
@@ -8,6 +8,8 @@ import com.booklog.auth.oauth.kakao.KakaoTokenDto;
 import com.booklog.auth.oauth.kakao.KakaoUserDto;
 import com.booklog.member.dto.MemberLoginDto;
 import com.booklog.member.entity.Member;
+import java.net.URI;
+import java.net.URISyntaxException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -28,18 +30,48 @@ public class AuthController {
     private final GoogleService googleService;
 
     @GetMapping("/kakaoLogin")
-    public ResponseEntity<MemberLoginDto> kakaoLogin(@RequestParam("code") String code) {
-        KakaoTokenDto kakaoTokenDto = kakaoService.getKakaoAccessToken(code);
-        KakaoUserDto kakaoUserDto = kakaoService.getKakaoUser(kakaoTokenDto.getAccessToken());
-        Member loginMember = kakaoService.loginKakao(kakaoUserDto);
-        return new ResponseEntity<>(kakaoService.getMemberLoginDto(loginMember), HttpStatus.OK);
+    public ResponseEntity<MemberLoginDto> kakaoLogin(@RequestParam("code") String code)
+        throws URISyntaxException {
+        try {
+            KakaoTokenDto kakaoTokenDto = kakaoService.getKakaoAccessToken(code);
+            KakaoUserDto kakaoUserDto = kakaoService.getKakaoUser(kakaoTokenDto.getAccessToken());
+            Member loginMember = kakaoService.loginKakao(kakaoUserDto);
+            URI redirectUri = new URI("http://localhost:3000");
+
+            // ResponseEntity를 사용하여 리다이렉트를 반환
+            return ResponseEntity.status(HttpStatus.FOUND)
+                .location(redirectUri)
+                .body(kakaoService.getMemberLoginDto(loginMember));
+        } catch (Exception e) {
+            URI errorRedirectUri = new URI("http://localhost:3000");
+
+            // ResponseEntity를 사용하여 에러 상태와 리다이렉트를 반환
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .location(errorRedirectUri)
+                .build();
+        }
     }
 
     @GetMapping("/googleLogin")
-    public ResponseEntity<MemberLoginDto> googleLogin(@RequestParam("code") String code) {
-        GoogleTokenDto googleTokenDto = googleService.getGoogleAccessToken(code);
-        GoogleUserDto googleUserDto = googleService.getGoogleUser(googleTokenDto.getAccessToken());
-        Member loginMember = googleService.loginGoogle(googleUserDto);
-        return new ResponseEntity<>(kakaoService.getMemberLoginDto(loginMember), HttpStatus.OK);
+    public ResponseEntity<MemberLoginDto> googleLogin(@RequestParam("code") String code)
+        throws URISyntaxException {
+        try{
+            GoogleTokenDto googleTokenDto = googleService.getGoogleAccessToken(code);
+            GoogleUserDto googleUserDto = googleService.getGoogleUser(googleTokenDto.getAccessToken());
+            Member loginMember = googleService.loginGoogle(googleUserDto);
+            URI redirectUri = new URI("http://localhost:3000");
+
+            // ResponseEntity를 사용하여 리다이렉트를 반환
+            return ResponseEntity.status(HttpStatus.FOUND)
+                .location(redirectUri)
+                .body(kakaoService.getMemberLoginDto(loginMember));
+        } catch (Exception e) {
+            URI errorRedirectUri = new URI("http://localhost:3000");
+
+            // ResponseEntity를 사용하여 에러 상태와 리다이렉트를 반환
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .location(errorRedirectUri)
+                .build();
+        }
     }
 }
